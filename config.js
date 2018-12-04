bf62b09178302e5d7754a765678b4a3c39c27d07
Heather <45602187+IHeatherB@users.noreply.github.com>
2018-12-04 13:52:42 -0500

    Added action timeout.

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ module.exports = {
     token: "YourBoatTokenHere",
     prefix: "!",
     owner: "YourUserIDHere",
-    modLogChannel: "mod-log"
+    modLogChannel: "mod-log",
+    actionTimeout: 500
 };
